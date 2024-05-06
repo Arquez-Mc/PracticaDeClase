da67ea52c03dc9238f4f993b6c48624dbbc4ac28
Arquez-Mc <163915164+Arquez-Mc@users.noreply.github.com>
2024-05-06 12:48:42 -0600

    Add files via upload

---
+++ Menu de restaurante.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+char letra=' ';
+
+
+while (letra !='X')
+{
+cout<<" Este es el menu del dia de hoy, escoga una letra\n";
+cout<<" A.Pizza\n";
+cout<<" B.Hamburguesa \n";
+cout<<" C.Tortas\n";
+cout<<" D. Pollo frito\n";
+cout<<" E.Tacos \n";
+
+cin>>letra;
+}  
+   return 0;
+}
---
+++ TablaDel12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+   int tabla=1;
+   //el while sirve para un bucle y en este bucle puedo darle caracterisrticas a una variables
+   while(tabla<13)
+   {
+       cout<<"12*"<<tabla<<"="<<12*tabla<<"\n";
+       tabla++;
+       
+   }
+   return 0;
+}
